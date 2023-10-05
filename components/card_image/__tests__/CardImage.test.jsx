bcb01b64ef5eb588641890be59d5949f0f4dfd17
Horea <constandacheh@gmail.com>
2023-10-05 18:41:27 +0200

    added tests for card image

--- components/card_image/__tests__/CardImage.test.jsx
+++ components/card_image/__tests__/CardImage.test.jsx
@@ -1,13 +1,15 @@
 import { render } from '@testing-library/react'
 import { AppContext } from '../../../pages'
 import CardImage from '../CardImage'
+import CardContainer from '../CardContainer'
 
 describe('Menu', () => {
     let state = {
         isDrawerOpen: false,
         isPopupOn: false
     }
-    it('should render corectly', () => {
+
+    it('should render correctly', () => {
         const image = {
                 width: 6000,
                 height: 4000,
@@ -17,6 +19,25 @@ describe('Menu', () => {
         const comp = render(
             <AppContext.Provider value={state}>
                 <CardImage image={image}/>
+            </ AppContext.Provider>
+        )
+
+        expect(comp).toMatchSnapshot()
+    })
+
+    it('should render correctly in the container', () => {
+        const images = [{
+                width: 6000,
+                height: 4000,
+                path: '/images/2.jpg',
+                alt: "cityscape"
+        }]
+        
+        const comp = render(
+            <AppContext.Provider value={state}>
+                <CardContainer>
+                    {images.map((image, el) => (<CardImage image={image} key={el}/>))}
+                </CardContainer>
             </ AppContext.Provider>
         )
 
