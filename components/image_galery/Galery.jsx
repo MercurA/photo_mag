682ea342410c3122672f1fdc6e7c408606abcc9d
Horea <constandacheh@gmail.com>
2023-10-13 01:19:59 +0200

    added design twicks

--- components/image_galery/Galery.jsx
+++ components/image_galery/Galery.jsx
@@ -13,53 +13,65 @@ const Galery = ({ dispatch }) => {
 
     useEffect(() => {
         const current = scrollContainerRef.current
+
+        const handleScroll = (e) => {
+            if (e.deltaY > 0) {
+                setItems((prevItems) => {
+                    dispatch({
+                        type: ACTIONS.setCurrentImageToDisplay,
+                        payload: prevItems[1]
+                    })
+                    return [
+                        ...prevItems.slice(1),
+                        prevItems[0]
+                    ]
+                });
+            } else if (e.deltaY < 0) {
+                setItems((prevItems) => {
+                    dispatch({
+                        type: ACTIONS.setCurrentImageToDisplay,
+                        payload: prevItems[prevItems.length - 1]
+                    })
+                    return [
+                        prevItems[prevItems.length - 1],
+                        ...prevItems.slice(0, -1),
+                    ]
+                });
+            }
+        }
+
         current.addEventListener('wheel', handleScroll)
 
         return () => current.removeEventListener('wheel', handleScroll)
-    },[items])
+    }, [items, dispatch])
 
-    const handleScroll = (e) => {
-        if (e.deltaY > 0) {
-            setItems((prevItems) => {
-                dispatch({
-                    type: ACTIONS.setCurrentImageToDisplay,
-                    payload: prevItems[1]
-                })
-                return [
-                    ...prevItems.slice(1),
-                    prevItems[0]
-                ]
-            });
-        } else if (e.deltaY < 0) {
-            setItems((prevItems) => {
-                dispatch({
-                    type: ACTIONS.setCurrentImageToDisplay,
-                    payload: prevItems[prevItems.length - 1]
-                })
-                return [
-                    prevItems[prevItems.length - 1],
-                    ...prevItems.slice(0, -1),
-                ]
-            });
-        }
-    }
+
 
     return (
-        <div
-            ref={scrollContainerRef}
-            className={styles.container}
-        >
-            {items.map((image, index) => (
-                <div className={styles.item} key={index} ref={itemRef}>
-                    <Image
-                        src={image}
-                        alt={''}
-                        fill={true}
-                        className='image'
-                    />
-                </div>
-            ))}
-        </div>
+        <>
+            <div
+                ref={scrollContainerRef}
+                className={styles.container}
+            >
+                {items.map((image, index) => (
+                    <div className={styles.item} key={index} ref={itemRef}>
+                        <div className={styles.rimRight}>
+                            {[1, 2, 3, 4].map((el) => (<div key={el} className={styles.rimHole}></div>))}
+                        </div>
+                        <Image
+                            src={image}
+                            alt={''}
+                            fill={true}
+                            className='image'
+                        />
+                        <div className={styles.rimLeft}>
+                            {[1, 2, 3, 4].map((el) => (<div key={el} className={styles.rimHole}></div>))}
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div></div>
+        </>
     )
 }
 
