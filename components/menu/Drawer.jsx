b6a6c99f1c3e10089b315442fdd533b16a050e4a
Horea <constandacheh@gmail.com>
2023-10-18 23:25:08 +0200

    added menu for mobile

--- components/menu/Drawer.jsx
+++ components/menu/Drawer.jsx
@@ -1,9 +1,10 @@
 import Image from 'next/image'
 import styles from './menu.module.css'
+import { strings } from '../../utils/constants'
+import Link from 'next/link'
 
-const LIST = ['Galery', 'Shop']
-
-const Drawer = ({ isOpen, handleDrawer }) => {
+const Drawer = ({ isOpen, handleDrawer, handleMenuSelection }) => {
+   
     return (
         <div className={`${styles.drawer} ${isOpen ? styles.drawerOpen : styles.drawerClosed}`}>
             <div className={styles.drawerHeader}>
@@ -12,9 +13,23 @@ const Drawer = ({ isOpen, handleDrawer }) => {
                 </button>
             </div>
             <div className={styles.drawerBody}>
-                {LIST.map((el, i) => (
-                    <div key={i} className={styles.drawerItem}>{el}</div>
+                {strings.selections.map((el, i) => (
+                    <div key={i} data-id={el.id} className={styles.drawerItem} onClick={handleMenuSelection}>{el.name}</div>
                 ))}
+            </div>
+            <div className={styles.drawerFooter}>
+            <p>constandacheh@gmail.com</p>
+                <div>
+                    <Link className={styles.link} href={'https://www.instagram.com/cypher_mercurius/'} target="_blank">
+                        <Image src={'/icons/insta_white.svg'} alt="instagram logo" width={20} height={20} />
+                    </Link>
+                    <Link className={styles.link} href="https://github.com/MercurA" target="_blank">
+                        <Image src={'/icons/github_white.svg'} alt="github logo" width={20} height={20} />
+                    </Link>
+                    <Link className={styles.link} href="https://soundcloud.com/dionis5" target="_blank">
+                        <Image src={'/icons/soundcloud.jpg'} alt="github logo" width={20} height={20} />
+                    </Link>
+                </div>
             </div>
         </div>
     )
