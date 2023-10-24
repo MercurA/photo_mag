d6b24bf287195907b36ae56bb5d4a612521f5873
Horea <constandacheh@gmail.com>
2023-10-24 22:18:07 +0200

    added story for galery

---
+++ stories/galery/data.js
@@ -0,0 +1,33 @@
+export const data = {
+    imageList: {
+        'black&white': [
+            {
+                path: '/images/arh_1.jpg',
+                width: 6016,
+                height: 4016
+            },
+            {
+                path: '/images/arh_2.jpg',
+                width: 6016,
+                height: 4016
+            },
+            {
+                path: '/images/3.jpg',
+                width: 6016,
+                height: 4016
+            },
+            {
+                path: '/images/4.jpg',
+                width: 6016,
+                height: 4016
+            },
+            {
+                path: '/images/land_2.jpg',
+                width: 4016,
+                height: 6016
+            }
+        ]
+    },
+    currentImageCollection: 'black&white',
+    dispath: () =>  {}
+}
