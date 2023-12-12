a9b0e49434d838add3d916c696d9ef2f4b1a0b5e
Horea <constandacheh@gmail.com>
2023-12-12 22:54:35 +0100

    start work on layout

--- pages/model/index.jsx
+++ pages/model/index.jsx
@@ -1,68 +1,33 @@
 import { useEffect, useRef, useState } from 'react'
-import styles from './model.module.css'
+import { useCursor, MeshReflectorMaterial, Image, Text, Environment } from '@react-three/drei'
 import { Canvas, useFrame, useThree } from '@react-three/fiber'
-import Controllers from '../../components/navigation/Controlers'
-import { CameraControls, OrbitControls } from '@react-three/drei'
-import * as THREE from "three";
-
-const Sphere = ({ position, size, color }) => {
-    const [hovered, setHovered] = useState(false)
-
-    const sphereRef = useRef()
-
-    useFrame((state, delta) => {
-        sphereRef.current.rotation.y += delta / 6
-    })
-
-    return (
-        <mesh ref={sphereRef} onPointerOver={(e) => { e.stopPropagation(); setHovered(!hovered) }}>
-            <sphereGeometry position={position} />
-            <meshStandardMaterial color={hovered ? color : 'lightblue'} wireframe />
-        </mesh>
-    )
-}
-
-const Scene = ({ cameraPos }) => {
-    const cameraControlsRef = useRef(new THREE.Vector3(0, 0, 0))
-    const { camera } = useThree();
-
-    useFrame((state, delta) => {
-        cameraControlsRef.current.moveTo(cameraPos[0],0,0, true)
-        console.log(state)
-    })
-
-    return (
-        <>
-            <group>
-                <directionalLight position={[0, 0, 7]} />
-                <Sphere position={[0, 0, 0]} size={[1, 1, 1]} color={'orange'} />
-                <CameraControls
-                    makeDefault
-                    ref={cameraControlsRef}
-                />
-            </group>
-        </>
-    )
-}
+import Frame from '../../components/3d_models/Frame'
 
 const Model = () => {
-    const [position, setPosition] = useState([0, 0, 0])
-
-    const handleDirection = (e) => {
-        if (e.target.id === 'right') {
-            setPosition([3, 0, 0])
-        } else if (e.target.id === 'left') {
-            setPosition([-3, 0, 0])
-        }
-
-    }
-
     return (
-        <div className={styles.container}>
-            <Controllers handleDirection={handleDirection} />
+        <div style={{height: '100vh'}}>
             <Canvas >
-            <ambientLight intensity={0.03} />
-                <Scene cameraPos={position} />
+                <color attach="background" args={['#191920']} />
+                <group position={[0, -0.5, 0]}>
+                    <Frame />
+                    <mesh rotation={[-Math.PI / 2, 0, 0]}>
+                        <planeGeometry args={[50, 50]} />
+                        <MeshReflectorMaterial
+                            blur={[300, 100]}
+                            resolution={2048}
+                            mixBlur={1}
+                            mixStrength={80}
+                            roughness={1}
+                            depthScale={1.2}
+                            minDepthThreshold={0.4}
+                            maxDepthThreshold={1.4}
+                            color="#050505"
+                            metalness={0.5}
+                        />
+
+                    </mesh>
+                </group>
+                <Environment preset="city" />
             </Canvas>
         </div>
     )
